4dade4e9f7db04c3a9f20753d45aed7fdcb08a23
amin <amin@contact.im>
2024-10-02 21:16:33 -0400

    Add files via upload

---
+++ more.js
@@ -0,0 +1,16 @@
+// more.js
+
+function copy() {
+  // Get the text field
+  var copyText = document.getElementById("txtInput");
+
+  // Select the text field
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); // For mobile devices
+
+   // Copy the text inside the text field
+  navigator.clipboard.writeText(copyText.value);
+
+  // Alert the copied text
+  alert("Copied the text: " + copyText.value);
+}
